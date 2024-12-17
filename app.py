2628bb3f420b6b725667ea58ab52381dbd43a73a
Jorge Benito Ugás <147073922+DoctorBIOS1990@users.noreply.github.com>
2024-12-17 08:03:15 +0100

    Added select
    Added select options filters, refactor css

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
+limit = '20'
+
+def constructJSON(categoryChannel):
+    dataObtain = requests.get('https://api.dailymotion.com/videos?channel='+categoryChannel+'&limit='+limit)
+    return dataObtain.json()
 
 @app.route('/')
 def index():
-    dataObtain = requests.get('https://api.dailymotion.com/videos?channel=shortfilms&limit=20')
-    dataJSON = dataObtain.json()
-    print(dataJSON)
+    dataJSON = constructJSON('tv')
+    return render_template('index.html', data = dataJSON['list'])
 
+@app.route('/category', methods=['POST'])
+def categoria():
+    categoria = request.form['categoria']
+    dataJSON = constructJSON(categoria)
     return render_template('index.html', data = dataJSON['list'])
 
 if __name__ == '__main__':
